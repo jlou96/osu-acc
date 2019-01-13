6b8d8744b34ef14243a008fc3c115fef867d6e26
Julian Lou <lou.julian96@gmail.com>
2019-01-13 05:07:21 -0800

    Implement basic app routing and create a home page for each app

--- osu_acc/urls.py
+++ osu_acc/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('beatmap/', include('beatmap.urls')),
+    path('player/', include('player.urls')),
+    path('rankings/', include('rankings.urls')),
+    path('replay/', include('replay.urls')),
 ]
